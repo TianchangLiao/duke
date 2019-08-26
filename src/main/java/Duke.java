e86f7fc5347f8e80b433f119170d02898a31de23
Tianchang Liao <tianchang_liao@hotmail.com>
2019-08-26 11:44:16 +0800

    Added ability to store user input and display back upon request

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] todo = new String[100];
+        int counter = 0;
         String logo = " ____        _        \n"
                     + "|  _ \\ _   _| | _____ \n"
                     + "| | | | | | | |/ / _ \\\n"
@@ -11,12 +13,21 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         while (true) {
-            String reply = sc.nextLine();
-            if (reply.equals("bye")) {
+            String task = sc.nextLine();
+            if (task.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (task.equals("list")) {
+                for (int i = 0; i < todo.length; i++) {
+                    if (todo[i] != null) {
+                        System.out.println(i + 1 + ". " + todo[i]);
+                    }
+                }
+            } else {
+                todo[counter] = task;
+                System.out.println("added: " + task);
+                counter++;
             }
-            System.out.println(reply);
         }
     }
 }
